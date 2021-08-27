9acf048b618e854d9b2e19ce0d110d9e9f828610
Golubova Viktoria <viki12007@inbox.ru>
2021-08-27 15:31:01 +0300

    animation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view />
+    <transition name="fade" mode="out-in">
+    <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -13,5 +15,13 @@ export default {
 <style>
 #app {
   
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-active,
+.fide-leave-active {
+  transition: opacity .5s ease-out;
 }
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -25,16 +25,24 @@
           v-if="canAddToCart(product)">Add to cart</button>
           <button disabled="true" class=" btn btn-primary btn-lg"
           v-else >Add to cart</button>
-          <span class="inventory-message"
-          v-if="product.availableInventory - cartCount(product.id) === 0">All Out!
-        </span>
-        <span class="inventory-message"
-        v-else-if="product.availableInventory - cartCount(product.id) < 5">
-        Only {{product.availableInventory - cartCount(product.id)}} left!
-      </span>
-      <span class="inventory-message"
-      v-else>Buy Now!
-    </span>
+          <transition mode="out-in" name="bounce">
+             <span class="inventory-message"
+             v-if="product.availableInventory - cartCount(product.id) === 0"
+             key="0"
+             >All Out!
+           </span>
+            <span class="inventory-message"
+             v-else-if="product.availableInventory - cartCount(product.id) < 5"
+             key=""
+             >
+             Only {{product.availableInventory - cartCount(product.id)}} left!
+           </span>
+           <span class="inventory-message"
+             v-else
+             key=""
+             >Buy Now!
+           </span>
+        </transition>
     <div class="rating">
       <span  
       v-bind:class="{'rating-active' :checkRating(n, product)}"
@@ -152,6 +160,26 @@ export default {
 </script>
 
 <style scoped>
-
+.bounce-enter-active {
+  animation: shake 0.72s cubic-bezier(.37, .07, .19, .97) both;
+  transform: translate3d(0, 0, 0);
+  backface-visibility: hidden;
+}
+@keyframes shake {
+  10%, 90% {
+    color: red;
+    transform: translate3d(-1px, 0, 0); 
+  }
+  20%, 80% {
+    transform: translate3d(2px, 0, 0); 
+  }
+  30%, 50%, 70% {
+    color: red;
+    transform: translate3d(-4px, 0, 0); 
+  }
+  40%, 60% {
+    transform: translate3d(4px, 0, 0); 
+  }
+}
 </style>
 
